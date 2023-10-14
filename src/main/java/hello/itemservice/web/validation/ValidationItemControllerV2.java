905cb425312c1ccfa4e0a2c683823dbeed88feac
EUIJOON <euijoonism@gmail.com>
2023-10-14 21:30:49 +0900

    Validator (2)

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -27,6 +29,12 @@ public class ValidationItemControllerV2 {
     private final ItemRepository itemRepository;
     private final ItemValidator itemValidator; // 추가
 
+    // 추가
+    @InitBinder
+    public void init(WebDataBinder dataBinder) {
+        log.info("init binder {}", dataBinder);
+        dataBinder.addValidators(itemValidator);
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -223,15 +231,35 @@ public class ValidationItemControllerV2 {
 //    }
 
 
-    @PostMapping("/add")
-    public String addItemV5(@ModelAttribute Item item,
-                            BindingResult bindingResult,
-                            RedirectAttributes redirectAttributes) {
-        // 딱 한줄로 축약 (위에 의존성 주입! private final ItemValidator itemValidator;)
-        itemValidator.validate(item, bindingResult);
+//    @PostMapping("/add")
+//    public String addItemV5(@ModelAttribute Item item,
+//                            BindingResult bindingResult,
+//                            RedirectAttributes redirectAttributes) {
+//        // 딱 한줄로 축약 (위에 의존성 주입! private final ItemValidator itemValidator;)
+//        itemValidator.validate(item, bindingResult);
+//
+//        if (bindingResult.hasErrors()) {
+//            log.info("errors={}", bindingResult);
+//            return "validation/v2/addForm";
+//        }
+//
+//        //성공 로직
+//        Item savedItem = itemRepository.save(item);
+//        redirectAttributes.addAttribute("itemId", savedItem.getId());
+//        redirectAttributes.addAttribute("status", true);
+//        return "redirect:/validation/v2/items/{itemId}";
+//    }
 
+
+    @PostMapping("/add")
+    public String addItemV6(@Validated @ModelAttribute Item item, //여기
+                            BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes,
+                            Model model) {
+
+        //검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
-            log.info("errors={}", bindingResult);
+            log.info("errors={} ", bindingResult);
             return "validation/v2/addForm";
         }
 
@@ -241,7 +269,6 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
-
 
 
     @GetMapping("/{itemId}/edit")
